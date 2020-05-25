e854088eb911defc847b4ff09bb5077a38c89dea
Zhaxxon <Zhardemov.Zh@gmail.com>
2020-05-25 18:05:08 +0500

    Greatest Common Divisor

---
+++ Python_1/Modules_and_Functions/Greatest_Common_Divisor.py
@@ -0,0 +1,17 @@
+# Problem Statement #
+# Write a findGCD function that takes in two numbers as input a and b and finds the greatest common divisor of the two.
+
+# My solution
+import math
+## Write your function here
+def findGCD(a, b):
+  return math.gcd(a, b)# return the gcd of numbers
+
+print(findGCD(12, 20))
+
+
+# Real solution
+import math
+def findGCD(a, b):
+  return math.gcd(a, b)
+print(findGCD(24, 18))
