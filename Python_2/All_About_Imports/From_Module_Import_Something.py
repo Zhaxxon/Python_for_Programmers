da0edfcc750fae7636b40004b723a684794c3470
Zhaxxon <Zhardemov.Zh@gmail.com>
2020-07-10 20:36:08 +0500

    Using from module import something

---
+++ Python_2/All_About_Imports/From_Module_Import_Something.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+# functools.lru_cache(*args)
+
+from os import *
+
+from os import path, walk ,unlink
+from os import uname, remove
+
+from os import (path, walk, unlink, uname, remove)
+
+from os import path walk, unlink, uname, \
+    remove, rename
