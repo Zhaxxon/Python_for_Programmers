b07586f3c5b9a8083da0125fe33552bbfdccecaf
Zhaxxon <Zhardemov.Zh@gmail.com>
2020-07-11 20:39:45 +0500

    import_from_github_com

---
+++ Python_2/The_Importlib_Module/import_from_github_com.py
@@ -0,0 +1,12 @@
+from github_com.zzzeek import sqlalchemy
+#Collecting git+https://github.com/zzzeek/sqlalchemy
+#  Cloning https://github.com/zzzeek/sqlalchemy to /tmp/pip-acfv7t06-build
+#Installing collected packages: SQLAlchemy
+#  Running setup.py install for SQLAlchemy ... done
+#Successfully installed SQLAlchemy-1.1.0b1.dev0
+
+print(locals())
+#{'sqlalchemy': <module 'sqlalchemy' from '/usr/local/lib/python3.5/dist-packages/sqlalchemy/__init__.py'>,
+#'__file__': '/usercode/__ed_file.py', '__package__': None, '__cached__': None, '__doc__': None,
+#'__spec__': None, '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x7fd93d1775c0>,
+#'__name__': '__main__', '__builtins__': <module 'builtins' (built-in)>}
