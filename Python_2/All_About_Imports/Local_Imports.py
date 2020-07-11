9265c5e2aa5a8f937cb4aee2b48e179639042cfc
Zhaxxon <Zhardemov.Zh@gmail.com>
2020-07-11 19:37:52 +0500

    Local Imports

---
+++ Python_2/All_About_Imports/Local_Imports.py
@@ -0,0 +1,15 @@
+# import math
+import sys # global scope
+
+def square_root(a):
+    # This import is into the square_root functions local scope
+    import math
+    return math.sqrt(a)
+
+def my_pow(base_num, power):
+    import math
+    return math.pow(base_num, power)
+
+if __name__ == '__main__':
+    print(square_root(49))
+    print(my_pow(2, 3))
