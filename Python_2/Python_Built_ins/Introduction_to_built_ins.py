07427a43fae0804a1458966ba81bfe156cff7cfb
Zhaxxon <Zhardemov.Zh@gmail.com>
2020-07-28 07:08:19 +0500

    Introduction to built-ins

---
+++ Python_2/Python_Built_ins/Introduction_to_built_ins.py
@@ -0,0 +1,10 @@
+print (any([0,0,0,1]))
+#True
+
+
+widget_one = ''
+widget_two = ''
+widget_three = 'button'
+widgets_exist = any([widget_one, widget_two, widget_three])
+print (widgets_exist)
+#True
