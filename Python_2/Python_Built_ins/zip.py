4df27bc8696764b72cd786fc34c95aeb23ff3657
Zhaxxon <Zhardemov.Zh@gmail.com>
2020-07-28 07:20:48 +0500

    Python Built-ins

---
+++ Python_2/Python_Built_ins/zip.py
@@ -0,0 +1,14 @@
+keys = ['x', 'y', 'z']
+values = [5, 6, 7]
+print(zip(keys, values))
+# <zip object at 0x7faaad4dd848>
+
+print(list(zip(keys, values)))
+# [('x', 5), ('y', 6), ('z', 7)]
+
+
+keys = ['x', 'y', 'z']
+values = [5, 6, 7]
+my_dict = dict(zip(keys, values))
+print (my_dict)
+#{'x': 5, 'y': 6, 'z': 7}
